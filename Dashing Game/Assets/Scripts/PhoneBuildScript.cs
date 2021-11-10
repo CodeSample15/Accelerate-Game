1f6a32bd67e415556b5fed7152747f2537da820c
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2021-11-10 11:10:27 -0500

    Control UI for phone or PC build

---
+++ Dashing Game/Assets/Scripts/PhoneBuildScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneBuildScript : MonoBehaviour
+{
+    [SerializeField] private bool ForPhone;
+    [SerializeField] private List<GameObject> ItemsToTurnOff;
+
+    void Awake()
+    {
+        foreach(GameObject item in ItemsToTurnOff)
+        {
+            item.SetActive(ForPhone);
+        }
+    }
+}
