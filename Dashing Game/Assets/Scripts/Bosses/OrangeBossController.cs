ac82bcb5dea8330114ca6107f555d15567792c8b
Luke <lukecrimi15@gmail.com>
2023-06-09 00:07:24 -0400

    Fixed orange boss eye
    Also started working on a turret

--- Dashing Game/Assets/Scripts/Bosses/OrangeBossController.cs
+++ Dashing Game/Assets/Scripts/Bosses/OrangeBossController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class OrangeBossController : MonoBehaviour
 {
     [Header("Cosmetic")]
+    [SerializeField] private GameObject eye;
     [SerializeField] private float minBlinkTime;
     [SerializeField] private float maxBlinkTime;
+    [SerializeField] private float eyeFocusDistance;
+    [SerializeField] private float maxEyeOffset;
+
+    private Player player;
 
     private bool isAttacking;
 
@@ -15,25 +20,50 @@ public class OrangeBossController : MonoBehaviour
     private float timeSinceLastBlink;
     private float nextBlinkTime;
 
+    private Vector2 eyeOffset;
+    private Vector2 eyeOffsetVel;
+
     void Awake()
     {
+        player = FindObjectOfType<Player>();
         anims = GetComponent<Animator>();
 
         isAttacking = false;
 
         timeSinceLastBlink = 0;
         nextBlinkTime = Random.Range(minBlinkTime, maxBlinkTime);
+
+        eyeOffset = Vector2.zero;
+        eyeOffsetVel = Vector2.zero;
     }
 
     void Update()
     {
-        timeSinceLastBlink += Time.deltaTime;
-
-        if (timeSinceLastBlink >= nextBlinkTime && !isAttacking)
+        if (!PauseButton.IsPaused)
         {
-            anims.SetTrigger("Blink");
-            timeSinceLastBlink = 0;
-            nextBlinkTime = Random.Range(minBlinkTime, maxBlinkTime);
+            //update the position of the eye
+            Vector2 target = Vector2.zero;
+            if (distanceToPlayer() > eyeFocusDistance)
+                target = (player.transform.position - transform.position).normalized * maxEyeOffset;
+            eyeOffset = Vector2.SmoothDamp(eyeOffset, target, ref eyeOffsetVel, 0.2f);
+
+            eye.transform.position = (Vector2)transform.position + eyeOffset;
+
+            //update the blinking animation
+            timeSinceLastBlink += Time.deltaTime;
+
+            if (timeSinceLastBlink >= nextBlinkTime && !isAttacking)
+            {
+                anims.SetTrigger("Blink");
+                timeSinceLastBlink = 0;
+                nextBlinkTime = Random.Range(minBlinkTime, maxBlinkTime);
+            }
         }
+    }
+
+    //helper method
+    private float distanceToPlayer()
+    {
+        return Vector2.Distance(transform.position, player.transform.position);
     }
 }
---
+++ Dashing Game/Assets/Scripts/Bosses/TurretController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float turnTime;
+    [SerializeField] private float fireTime;
+
+    private Player player;
+    private Animator anims;
+    private bool active;
+
+    //where the turret should be pointing
+    private Vector2 turn;
+    private Vector2 turnVel;
+
+    //how much time has elapsed between each shot
+    private float shotTimer;
+
+    void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        anims = GetComponent<Animator>();
+        active = false;
+    }
+
+    void Start()
+    {
+        StartCoroutine(startup());
+    }
+
+    void Update()
+    {
+        if(PauseButton.IsPaused && active)
+        {
+            Vector2 target = player.transform.position - transform.position;
+            turn = Vector2.SmoothDamp(turn, target, ref turnVel, turnTime);
+
+            shotTimer += Time.deltaTime;
+
+            if(shotTimer >= fireTime)
+            {
+                anims.SetTrigger("Fire");
+
+                //spawn bullet
+
+                shotTimer = 0f;
+            }
+        }
+    }
+
+    IEnumerator startup()
+    {
+        yield return new WaitForSeconds(1f);
+
+        active = true;
+    }
+}
