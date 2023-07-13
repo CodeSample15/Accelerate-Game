6a81af0d9a619f8021a54e10c025e6501d83da5a
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2023-07-13 18:36:12 -0400

    Tutorial logic set in place

--- Dashing Game/Assets/Scripts/Controllers/TutorialController.cs
+++ Dashing Game/Assets/Scripts/Controllers/TutorialController.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class TutorialController : MonoBehaviour
 {
     public static TutorialController staticRef;
-
+    
     [Header("UI Items")]
     [SerializeField] private GameObject HealthBar;
 
@@ -27,11 +27,14 @@ public class TutorialController : MonoBehaviour
 
     [Header("Explanations")]
     [SerializeField] private TextMeshProUGUI ExplanationText;
+    [SerializeField] private Animator textAnimation;
 
     [Space]
 
     [Header("Enemy")]
     [SerializeField] private GameObject Enemy;
+
+    private bool usingController;
 
     void Awake()
     {
@@ -45,10 +48,70 @@ public class TutorialController : MonoBehaviour
         ExplanationText.SetText("");
 
         staticRef = this;
+
+        usingController = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow))
+            usingController = false;
+        else if (Input.GetAxis("JoystickChecker") > 0)
+            usingController = true;
     }
 
     public void Triggered(string name)
     {
+        switch(name)
+        {
+            case "Start":
+                ExplanationText.SetText("Use arrow keys or joystick to move");
+                ExplanationText.rectTransform.position = new Vector2(-3.9f, 46.4f);
+                textAnimation.SetTrigger("Blink");
+                break;
 
+            case "Jump":
+                if (usingController)
+                    ExplanationText.SetText("Press \"A\" to jump");
+                else
+                    ExplanationText.SetText("Press space to jump");
+                ExplanationText.rectTransform.position = new Vector2(21.49f, 46.04f);
+                textAnimation.SetTrigger("Blink");
+                break;
+
+            case "Double Jump":
+                ExplanationText.SetText("Press jump while in the air to double jump");
+                textAnimation.SetTrigger("Blink");
+                break;
+
+            case "Wall Jump":
+                break;
+
+            case "Health":
+                break;
+
+            case "Dash":
+                break;
+
+            case "Red Enemy":
+                break;
+
+            case "Green Enemy":
+                break;
+
+            case "Blue Enemy":
+                break;
+
+            case "Yellow Enemy":
+                break;
+
+            case "Recharge Box":
+                break;
+
+            case "Crystal":
+                break;
+
+            
+        }
     }
 }
